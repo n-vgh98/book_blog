af5529cc1ae5b10bbd74a62f85e086cc27a7f7ef
n-vgh98 <nazi.gh988@gmail.com>
2023-02-21 17:25:02 +0330

    Add list and detail for books

--- books/urls.py
+++ books/urls.py
@@ -4,4 +4,5 @@ from .views import *
 
 urlpatterns = [
     path('lists', list_books, name='list_books'),
+    path('detail/<int:pk>', detail_book, name='detail_book')
 ]
--- books/views.py
+++ books/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import *
 
 
 def list_books(request):
-    return render(request, 'list_books.html')
+    books = Book.objects.filter(is_publish=True)
+    authors = Author.objects.all()
+    context = {
+        'ketabha': books,
+        'authors': authors
+    }
+    return render(request, 'books/list_books.html', context)
+
+
+def detail_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    context = {
+        'book': book
+    }
+    return render(request, 'books/detail_book.html', context)
