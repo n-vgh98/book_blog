d1ebfdfe3f8691cceaf407aa6eaf86af1639fd45
n-vgh98 <nazi.gh988@gmail.com>
2023-02-21 11:23:32 +0330

    Create Book and Author models

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class AdminBook(admin.ModelAdmin):
+    list_display = ('title', 'status', 'is_publish')
+    list_filter = ('status', 'is_publish')
+
+
+class AdminAuthor(admin.ModelAdmin):
+    list_display = ('name', 'category')
+    list_filter = ('status',)
+
+
+admin.site.register(Book, AdminBook)
+admin.site.register(Author, AdminAuthor)
--- books/models.py
+++ books/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=64)
+    birth_date = models.DateField(null=True, blank=True)
+    category = models.CharField(max_length=30)
+    status = models.BooleanField(default=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=64)
+    page_number = models.PositiveSmallIntegerField(default=10)
+    content = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.DO_NOTHING, related_name='author_books')
+    is_publish = models.BooleanField(default=True)
+    # TODo image
+    status = models.BooleanField(default=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
